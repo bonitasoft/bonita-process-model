fb58550d8cf8ea8f71b4c7b0a08344c06578efaa
Vincent Hémery <vincent.hemery@bonitasoft.com>
2023-07-07 16:44:11 +0200

    Fixing Sonar issues

--- bundles/org.bonitasoft.bonita2bpmn/src/org/bonitasoft/bonita2bpmn/util/Strings.java
+++ bundles/org.bonitasoft.bonita2bpmn/src/org/bonitasoft/bonita2bpmn/util/Strings.java
@@ -1,16 +1,14 @@
-/** 
+/**
  * Copyright (C) 2021 Bonitasoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,7 +23,7 @@ public class Strings {
 
     private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
     private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
-    private static final Pattern EDGESDHASHES = Pattern.compile("(^-|-$)");
+    private static final Pattern EDGESDHASHES = Pattern.compile("(^-)|(-$)");
 
     /**
      * @param string
--- bundles/org.bonitasoft.bpm.model/src/org/bonitasoft/bpm/model/process/util/migration/MigrationHelper.java
+++ bundles/org.bonitasoft.bpm.model/src/org/bonitasoft/bpm/model/process/util/migration/MigrationHelper.java
@@ -1,16 +1,14 @@
-/** 
+/**
  * Copyright (C) 2022 Bonitasoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -168,6 +166,8 @@ public class MigrationHelper {
              */
             final ContentHandler contentHandler = new ContentHandler();
             SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+            // to be compliant with Sonar's java:S2755, completely disable DOCTYPE declaration:
+            parserFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
             parserFactory.setNamespaceAware(true);
             XMLReader reader = parserFactory.newSAXParser().getXMLReader();
 
