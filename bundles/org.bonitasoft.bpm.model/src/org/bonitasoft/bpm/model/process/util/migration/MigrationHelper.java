d3ca186e6505019c81ea96cce8550dd8b2fcbfa7
Vincent HEMERY <vincent.hemery@bonitasoft.com>
2023-07-25 10:42:24 +0200

    feat(model): Push to Bonita Process Model functionalities to build .bar (#25)
    feat(model): Push to Bonita Process Model functionalities to build .bar
    
    Fix Model migration memory leak and BPMN resource loading.
    
    Relates to [DEV-403](https://bonitasoft.atlassian.net/browse/DEV-403)

--- bundles/org.bonitasoft.bpm.model/src/org/bonitasoft/bpm/model/process/util/migration/MigrationHelper.java
+++ bundles/org.bonitasoft.bpm.model/src/org/bonitasoft/bpm/model/process/util/migration/MigrationHelper.java
@@ -73,11 +73,6 @@ public class MigrationHelper extends AdapterImpl {
      */
     public static final String OPTION_MIGRATION_POLICY = "MIGRATION_POLICY";//$NON-NLS-1$
 
-    /** Removes unloaded resources */
-    private static final AdapterImpl UNLOAD_LISTENER = new AdapterImpl() {
-
-    };
-
     /**
      * Get a helper for migration
      * 
@@ -127,8 +122,6 @@ public class MigrationHelper extends AdapterImpl {
         modelVersionStatus = compareModelVersions(resource.getURI().lastSegment());
         // init the migrator
         initializeMigrator(resource);
-        // remember to remove the migrator when the resource is disposed, or we will keep a reference to it forever
-        resource.eAdapters().add(UNLOAD_LISTENER);
     }
 
     /*
--- bundles/org.bonitasoft.spec.bpmn/src-gen/org/omg/spec/bpmn/di/util/DiResourceFactoryImpl.java
+++ bundles/org.bonitasoft.spec.bpmn/src-gen/org/omg/spec/bpmn/di/util/DiResourceFactoryImpl.java
@@ -15,17 +15,15 @@
 package org.omg.spec.bpmn.di.util;
 
 import org.eclipse.emf.common.util.URI;
-
 import org.eclipse.emf.ecore.resource.Resource;
-
 import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
-
 import org.eclipse.emf.ecore.xmi.XMLResource;
 
 /**
  * <!-- begin-user-doc -->
  * The <b>Resource Factory</b> associated with the package.
  * <!-- end-user-doc -->
+ * 
  * @see org.omg.spec.bpmn.di.util.DiResourceImpl
  * @generated
  */
@@ -35,6 +33,7 @@ public class DiResourceFactoryImpl extends ResourceFactoryImpl {
      * Creates an instance of the resource factory.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     public DiResourceFactoryImpl() {
@@ -45,7 +44,8 @@ public class DiResourceFactoryImpl extends ResourceFactoryImpl {
      * Creates an instance of the resource.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * 
+     * @generated NOT Ignore Unknown Feature
      */
     @Override
     public Resource createResource(URI uri) {
@@ -59,6 +59,10 @@ public class DiResourceFactoryImpl extends ResourceFactoryImpl {
         result.getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
 
         result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_LEXICAL_HANDLER, Boolean.TRUE);
+
+        //Ignore Unknown Feature
+        result.getDefaultLoadOptions().put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
+
         return result;
     }
 
