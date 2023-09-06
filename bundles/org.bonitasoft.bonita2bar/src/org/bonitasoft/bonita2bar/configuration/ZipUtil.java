bdae6abedea1b89a502599c37ec1e4f31c614c13
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-09-06 12:29:43 +0200

    fix(zip): normalize entry path

--- bundles/org.bonitasoft.bonita2bar/src/org/bonitasoft/bonita2bar/configuration/ZipUtil.java
+++ bundles/org.bonitasoft.bonita2bar/src/org/bonitasoft/bonita2bar/configuration/ZipUtil.java
@@ -81,7 +81,7 @@ public class ZipUtil extends SimpleFileVisitor<Path> implements java.lang.AutoCl
      * So the client doesn't have to manage this.
      */
     public static String normalizePath(Path path) {
-        return path.toString().replace("\\\\", "/");
+        return path.toString().replace(File.separator, "/");
     }
 
     @Override
